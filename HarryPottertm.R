ef8ac42c05abcf16dc68952d61e7164deca41bfe
nacnudus <nacnudus@gmail.com>
2013-04-27 13:34:25 +1200

    set up a data frame

--- HarryPottertm.R
+++ HarryPottertm.R
@@ -5,6 +5,21 @@
 # Also look at lda which can do word counts of documents.
 # UPDATE: use stringr:str_join like Para <- str_join(Thousand, collapse = " ")
 require(tm)
+require(stringr)
+
+# set up a data frame
+BookNames <- c(
+  "PS"
+  , "CS"
+  , "PA"
+  , "GF"
+  , "OP"
+  , "HP"
+  , "DH"
+)
+dfBooks <- data.frame(Name = BookNames)
+rownames(dfBooks) <- dfBooks$Name
+
 cCorpus <- Corpus(DirSource("/home/nacnudus/R/HarryPotter/Texts/"))
 cCorpus$Name <- c(
   "PS"
@@ -16,11 +31,7 @@ cCorpus$Name <- c(
   , "DH"
 )
 names(cCorpus) <- cCorpus$Name
-cCorpus$FirstLine <- sapply(cCorpus, function(x) (which(str_detect(x, "CHAPTER ONE"))[1]))
 
-cCorpus <- tm_map(cCorpus, removePunctuation)
-cCorpus <- tm_map(cCorpus, tolower)
-cCorpus <- tm_map(cCorpus, stripWhitespace)
 # Find the first line (there may be two or more because of 
 # samples from the other books).
 FirstLine <- sapply(cCorpus, function(x) (which(str_detect(x, "CHAPTER ONE"))[1]))
@@ -32,3 +43,13 @@ FinalLine <- sapply(cCorpus, function(x) (which(x == "Titles available in the Ha
 cCorpus <- sapply(seq(1, 7, 1), function(x ,y ,z) (cCorpus[[x]][y[x]:z[x]]), y = FirstLine, z = FinalLine)
 # Tidy up
 rm(FirstLine, FinalLine)
+
+# Get all the chapter headings and their row numbers
+ChapterHeading <- sapply(cCorpus, function(x) (which(str_detect(x, "CHAPTER"))))
+
+# Begin a dataframe for the books
+dfBooks <- data.frame(ChapterHeading=ChapterHeading, row.names=cCorpus$Name) 
+
+cCorpus <- tm_map(cCorpus, removePunctuation)
+cCorpus <- tm_map(cCorpus, tolower)
+cCorpus <- tm_map(cCorpus, stripWhitespace)
